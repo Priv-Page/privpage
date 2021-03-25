4c7242d5ecd3d8d2c4a7c1858a9c7a3449b114e5
Julien Reichardt <git@jrei.ch>
2021-03-25 15:22:17 +0100

    Add domain to logs

--- src/privpage.cr
+++ src/privpage.cr
@@ -15,11 +15,16 @@ module PrivPage
     context.response.headers["X-Content-Type-Options"] = "nosniff"
     # Disallow the site to be rendered within a frame (clickjacking protection)
     context.response.headers["X-Frame-Options"] = "sameorigin"
-    context.response.headers["Content-Security-Policy"] = "frame-ancestors 'self';"
+    context.response.headers["Content-Security-Policy"] = "frame-ancestors 'self'"
+
+    host = context.request.headers["Host"]?.to_s
 
     # For now only GitHub is supported
-    first_subdomain_part, root_domain = UserRepository.split_first_subdomain_part context.request.host.to_s
+    first_subdomain_part, root_domain = UserRepository.split_first_subdomain_part host
     GitHub.handle_request first_subdomain_part, root_domain, context
+
+    # log the domain along with the path
+    context.request.path = host + context.request.path.lchop '/'
   rescue ex
     context.response.respond_with_status(:internal_server_error)
     @@log.error(exception: ex) { }
